8fd2f69901ac56a0d63a098b30c69d30db525da7
yamunan-bitset <yamunan.bitset@gmail.com>
2021-09-11 13:36:54 +0000

    first line

---
+++ Makefile
@@ -0,0 +1,14 @@
+CXX      = c++
+LDFLAGS  = -lsfml-window -lsfml-system -lsfml-graphics
+CXXFLAGS = -g -std=c++17
+OBJS     = main.o
+OUT      = tictactoe
+
+all:     build
+build:   compile link
+compile: $(OBJS)
+link:
+	$(CXX) $(OBJS) $(CXXFLAGS) $(LDFLAGS) -o $(OUT)
+clean:
+	rm -rf $(OUT) $(OBJS)
+
--- main.cc
+++ main.cc
@@ -1,35 +1,39 @@
 #include <SFML/Graphics.hpp>
 
-#include "board.hh"
-
-int main(int argc, char** argv)
+int main(int argc, char** argv, char** envp)
 {
-	sf::RenderWindow window(sf::VideoMode(800, 800), "Tic Tac Toe");
-	/*
-	sf::Texture x, o;
-	x.loadFromFile("x.png");
-	o.loadFromFile("o.png");
-	sf::Sprite x_s(x), o_s(o);
-	*/
-	Board board;
-	
-	sf::Event event;
-	while (window.isOpen())
+  sf::RenderWindow window(sf::VideoMode(800, 800), "Tic Tac Toe");
+  sf::Clock clock;
+  
+  sf::Texture x, o;
+  x.loadFromFile("x.png");
+  o.loadFromFile("o.png");
+  sf::Sprite x_s(x), o_s(o);
+  
+  sf::VertexArray line(sf::Lines, 2);
+  line[0].position = sf::Vector2f(0, 0);
+  line[0].color = sf::Color(100, 0, 200);
+  line[1].position = sf::Vector2f(100, 100);
+  line[1].color = sf::Color(100, 0, 200);
+
+  sf::Event event;
+  while (window.isOpen())
+    {
+      while (window.pollEvent(event))
 	{
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-				case sf::Event::Closed:
-					window.close();
-					break;
-				default: break;
-			}
-		}
-		
-		window.draw(board);
-		window.clear();
-		window.display();
+	  switch (event.type)
+	    {
+	    case sf::Event::Closed:
+	      window.close();
+	      break;
+	    default: break;
+	    }
+	}
+
+      window.clear();
+      window.draw(line);
+      window.display();
     }
+
   return 0;
 }
