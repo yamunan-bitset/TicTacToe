5fbb96affc40dbd739cb64088bbb27cfedaed122
yamunan-bitset <yamunan.bitset@gmail.com>
2021-10-09 17:15:56 +0000

    added Computer // TODO

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 #include "sprite.hh"
 #include "board.hh"
@@ -11,11 +13,10 @@ int main(int argc, char** argv, char** envp)
   
   sf::RenderWindow window(sf::VideoMode(900, 900), "Tic Tac Toe");
 
-  bool draw_x = false, draw_o = false;
+  bool add_o = false, player_turn = false;
   float x_pos, y_pos;
   std::vector<sf::CircleShape> circles;
-  std::vector<float> xX;
-  std::vector<float> yX;
+  std::vector<std::pair<float, float>> computer_xy;
   
   sf::Event event;
   while (window.isOpen())
@@ -30,17 +31,13 @@ int main(int argc, char** argv, char** envp)
 	    case sf::Event::MouseButtonPressed:
 	      switch (event.mouseButton.button)
 		{
-		case sf::Mouse::Right:
-		  draw_x = true;
-		  draw_o = false;
-		  x_pos  = event.mouseButton.x;
-		  y_pos  = event.mouseButton.y;
-		  break;
 		case sf::Mouse::Left:
-		  draw_o = true;
-		  draw_x = false;
-		  x_pos = event.mouseButton.x;
-		  y_pos = event.mouseButton.y;
+		  if (player_turn)
+		    {
+		      add_o = true;
+		      x_pos = event.mouseButton.x;
+		      y_pos = event.mouseButton.y;
+		    }
 		  break;
 		default: break;
 		}
@@ -61,28 +58,58 @@ int main(int argc, char** argv, char** envp)
       if (x_pos >= 600 && x_pos <= 900) x_corrected = 750;
       if (y_pos >= 600 && y_pos <= 900) y_corrected = 750;
 
-      if (draw_x)
-	{
-	  xX.push_back(x_corrected);
-	  yX.push_back(y_corrected);
-	  draw_x = false;
-	}
-      
-      if (draw_o)
+      // Player Turn
+      if (add_o)
 	{
 	  circles.push_back(CreateO(x_corrected - 50, y_corrected - 50)); // 50 px offset
-	  draw_o = false;
+	  add_o = false;
+	  player_turn = false;
 	}
-      
-      for (auto i : xX)
+
+      // Computer
+      if (!player_turn)
 	{
-	  for (auto j : yX)
+	  std::srand(std::time(NULL));
+	  float x, y;
+	  // Choose Random Position between: 
+	  switch (1 + (std::rand() % 4))
 	    {
-	      window.draw(CreateX1(i, j));
-	      window.draw(CreateX2(i, j));
-	      window.draw(CreateX3(i, j));
-	      window.draw(CreateX4(i, j));
+	      //  X |  |
+	      // ---|--|---
+	      //    |  |
+	      // ---|--|---
+	      //    |  |
+	    case 1: x = 150; y = 150; break;
+	      //    |  |
+	      // ---|--|---
+	      //    |  |
+	      // ---|--|---
+	      //  X |  |
+	    case 2: x = 150; y = 750; break;
+	      //    |  | X
+	      // ---|--|---
+	      //    |  |
+	      // ---|--|---
+	      //    |  |
+	    case 3: x = 750; y = 150; break;
+	      //    |  |
+	      // ---|--|---
+	      //    |  |
+	      // ---|--|---
+	      //    |  | X
+	    case 4: x = 750; y = 750; break;
 	    }
+	  computer_xy.push_back(std::pair<float, float> (x, y));
+	  player_turn = true;
+	}
+
+      // Render
+      for (unsigned i = 0; i < computer_xy.size(); i++)
+	{
+	  window.draw(CreateX1(computer_xy[i].first, computer_xy[i].second));
+	  window.draw(CreateX2(computer_xy[i].first, computer_xy[i].second));
+	  window.draw(CreateX3(computer_xy[i].first, computer_xy[i].second));
+	  window.draw(CreateX4(computer_xy[i].first, computer_xy[i].second));
 	}
       for (auto i : circles)
 	window.draw(i);
